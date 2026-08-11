08cdb46fccd011a190faf0dc72e0b87bbe347f40
crack69-git <tanchangyaashutosh0356@gmail.com>
2026-08-11 21:25:01 +0600

    added navbar

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import NavbarSection from "@/components/mainpage/NavbarSection";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <NavbarSection />
+      this is homepage
+    </div>
+  );
+}
---
+++ src/components/mainpage/NavbarSection.jsx
@@ -0,0 +1,32 @@
+import { Button } from "@heroui/react";
+import React from "react";
+
+const NavbarSection = () => {
+  return (
+    <div className="border-b">
+      <div className="flex justify-between items-center w-11/12 mx-auto py-5 ">
+        <div className="flex items-center gap-2">
+          <h1 className="text-3xl font-bold mr-5 text-green-700">
+            Crowd Fundly
+          </h1>
+          <div className="flex items-center gap-4">
+            <p className="font-semibold">Explore Campaign</p>
+            <p>How it works</p>
+            <p>Plattform impacts</p>
+          </div>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Button variant="" className="hover:bg-green-100">
+            Login
+          </Button>
+          <Button variant="primary" className="bg-green-700">
+            Join as Developer
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavbarSection;
